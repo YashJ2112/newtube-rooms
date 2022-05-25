4bf82c5b294ca512cb01efac2f68800b8d514d46
YashJagwani <yashjagwani2112@gmail.com>
2022-05-25 18:43:01 +0200

    added configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { env } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(env.MONGO_URI, {
       useNewUrlParser: true,
     }),
